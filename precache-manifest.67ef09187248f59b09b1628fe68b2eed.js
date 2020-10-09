fb90274e31d3d7d0ff13fe53280519b960747d93
rizwanishaq <rizwanishaq@gmail.com>
2020-10-09 10:09:02 +0200

    Updates

---
+++ precache-manifest.67ef09187248f59b09b1628fe68b2eed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0e3a5658a5d9fe25f59be735e1efc7",
+    "url": "/react-firestore-example/index.html"
+  },
+  {
+    "revision": "9b3cd547dd9b43d9619f",
+    "url": "/react-firestore-example/static/css/main.558bb6bf.chunk.css"
+  },
+  {
+    "revision": "8bc3b38978757f4e8124",
+    "url": "/react-firestore-example/static/js/2.614e3c13.chunk.js"
+  },
+  {
+    "revision": "da1ddcda7183b6313ab6bca1da290ddd",
+    "url": "/react-firestore-example/static/js/2.614e3c13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b3cd547dd9b43d9619f",
+    "url": "/react-firestore-example/static/js/main.f1894cb1.chunk.js"
+  },
+  {
+    "revision": "d9a2f38ac212b313912d",
+    "url": "/react-firestore-example/static/js/runtime-main.7a71ba96.js"
+  }
+]);
